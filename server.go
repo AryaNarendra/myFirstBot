7a5477975540675298719694027fbd209e2dfb41
sayak9495 <sayak1997sen@gmail.com>
2022-03-26 23:41:44 +0530

    Added a basic QnA in the boilerPlate

--- server.go
+++ server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/strike-official/go-sdk/strike"
 )
 
 type Strike_Meta_Request_Structure struct {
@@ -54,11 +56,8 @@ type GeoLocation_struct struct {
 }
 
 type User_session_variables_struct struct {
-	TextInput     string             `json:"textInput"`
-	LocationInput GeoLocation_struct `json:"locationInput"`
-	NumberInput   string             `json:"numberInput"`
-	DateInput     []string           `json:"dateInput"`
-	Card          []string           `json:"card"`
+	UserName string `json:"name"`
+	UserAge  string `json:"age"`
 }
 
 type AppConfig struct {
@@ -68,19 +67,65 @@ type AppConfig struct {
 
 var conf *AppConfig
 
+// This will be your API base link. Below we have used ngrok to make our bot public fast.
+var baseAPI = "http://8e28-2405-201-a407-908e-4c-9fe9-ad8b-43c8.ngrok.io"
+
 func main() {
 	conf = &AppConfig{Port: ":7001", APIEp: ""}
 	// Init Routes
 	router := gin.Default()
-	router.POST("/strike", Getting_started)
+	router.POST("/askDetails", GettingStarted)
+	router.POST("/respondBack", RespondBack)
 
 	// Start serving the application
 	err := router.Run(conf.Port)
 	if err != nil {
 		log.Fatal("[Initialize] Failed to start server. Error: ", err)
 	}
+
 }
 
-func Getting_started(ctx *gin.Context) {
-	ctx.JSON(200, "")
+func GettingStarted(ctx *gin.Context) {
+	var request Strike_Meta_Request_Structure
+	if err := ctx.BindJSON(&request); err != nil {
+		fmt.Println("Err")
+	}
+	// Core Logic
+	strikeObj := strike.Create("getting_started", baseAPI+"/respondBack")
+
+	// First Question: Whats your name?
+	quesObj1 := strikeObj.Question("name").
+		QuestionText().
+		SetTextToQuestion("Hi! What is your name?", "")
+	// Prompt the user to give his answer as a text.
+	quesObj1.Answer(true).TextInput("")
+
+	// Second Question: Whats your age?
+	quesObj2 := strikeObj.Question("age").
+		QuestionText().
+		SetTextToQuestion("What is you age", "desc")
+	// Prompt the user to give his answer as a number.
+	quesObj2.Answer(true).NumberInput("")
+
+	ctx.JSON(200, strikeObj)
+}
+
+func RespondBack(ctx *gin.Context) {
+	var request Strike_Meta_Request_Structure
+	if err := ctx.BindJSON(&request); err != nil {
+		fmt.Println("Err")
+	}
+
+	name := request.User_session_variables.UserName
+	age := request.User_session_variables.UserAge
+
+	// Core Logic (Not giving any return API as this is the last response to the User.)
+	strikeObj := strike.Create("getting_started", "")
+
+	// Respond back
+	strikeObj.Question("").
+		QuestionText().
+		SetTextToQuestion("Hi! "+name+" You are the choosen few. Congratulation on such a feat at the age of "+age+".", "")
+
+	ctx.JSON(200, strikeObj)
 }
